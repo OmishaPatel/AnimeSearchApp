00f6b1413d97491adcf0478e194130cbcc41c97f
Omisha Patel <64633546+OmishaPatel@users.noreply.github.com>
2021-03-22 01:49:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,4 +28,3 @@ results.splice(0, results.length);
 //to clear the innerHTML once the user deletes the search input
 cards.innerHTML = '';
 }
-// This is a Anime Search App build with HTML5, CSS3 and Vanilla Javascript. I am using fetch to access the data from the jikan api. This is also mobile responsive. I have limited the display to 10 items per page.
